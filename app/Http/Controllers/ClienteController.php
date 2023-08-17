2304e1ddf33ab3cddf2cae1b94f7b415c812ba1a
LucasRonaldo <lucasronaldoalvesdasilva@gmail.com>
2023-08-17 15:45:54 -0300

    Atualizando Cliente Controller

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ClienteFormRequest;
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class ClienteController extends Controller
+{
+    public function cliente(ClienteFormRequest $request){
+
+        $user = Cliente::create([
+          
+                'nome' => $request->nome,
+                'cpf' => $request->cpf,
+                'celular' => $request->celular,
+                'email' => $request->email,
+                'password'=> Hash::make($request->password)
+                
+        ]);
+        return response()->json([
+            "success" => true,
+            "message" => "Cliente Cadastrado com sucesso",
+            "data" => $user
+            
+        ], 200);
+    }
+}
